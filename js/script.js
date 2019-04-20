22ca679430a96aecfb2cf5cb151160a5b445cd50
RichardHarleyson <richard.harleyson@gmail.com>
2019-04-20 22:05:22 +0300

    mobile menu

--- js/script.js
+++ js/script.js
@@ -30,4 +30,32 @@ $(document).ready(function(){
 								 $(".section > div > input").val("1");
 						 }
 				 })
+
+				 // =======================================================
+
+				 $('#11').hover(function() {
+				 	$('.to_hide').hide();
+				 	$('.menu_items_11').show();
+				 });
+
+				 $('#12').hover(function() {
+				 	$('.to_hide').hide();
+				 	$('.menu_items_12').show();
+				 });
+
+				 $('#13').hover(function() {
+				 	$('.to_hide').hide();
+				 	$('.menu_items_13').show();
+				 });
+
+				 $('#21').hover(function() {
+				 	$('.to_hide').hide();
+				 	$('.menu_items_21').show();
+				 });
+
+				 $('#22').hover(function() {
+				 	$('.to_hide').hide();
+				 	$('.menu_items_22').show();
+				 });
+
 		 })
