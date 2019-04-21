3d0801ca448f3e5535a42cbb94a2b27df6b98cf9
RichardHarleyson <richard.harleyson@gmail.com>
2019-04-21 20:36:20 +0300

    2nd menu mobile version

--- js/script.js
+++ js/script.js
@@ -58,4 +58,20 @@ $(document).ready(function(){
 				 	$('.menu_items_22').show();
 				 });
 
+				 $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+				  if (!$(this).next().hasClass('show')) {
+				    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+				  }
+				  var $subMenu = $(this).next(".dropdown-menu");
+				  $subMenu.toggleClass('show');
+
+
+				  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+				    $('.dropdown-submenu .show').removeClass("show");
+				  });
+
+
+				  return false;
+				});
+
 		 })
